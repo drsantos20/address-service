30f2102d549261aff0cb000140f3d69b657594e0
Daniel Ribeiro dos Santos <drsantos20@gmail.com>
2019-12-19 09:43:48 -0300

    Clean code

--- api/address/utils.py
+++ api/address/utils.py
@@ -5,10 +5,12 @@ def build_google_maps_url(body, google_api_key):
     city = body['city']
     neighborhood = body['neighborhood']
     uf = body['uf']
+
     street = street.replace(' ', '+')
     city = city.replace(' ', '+')
     neighborhood = neighborhood.replace(' ', '+')
     uf = uf.replace(' ', '+')
+
     url = 'https://maps.googleapis.com/maps/api/geocode/json?address={}+{}+{}+{}&key={}'.format(
         street,
         neighborhood,
