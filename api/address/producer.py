1649b79163d5e4cf1177b35b677c3a3b83e5306c
Daniel Ribeiro dos Santos <drsantos20@gmail.com>
2019-12-20 00:56:32 -0300

    Remove producer and add database address model update-task wherever a new google response arrieve

--- api/address/producer.py
+++ api/address/producer.py
@@ -1,34 +1,14 @@
-from django.conf import settings
-from kombu import Connection, Exchange, Producer, Queue
 from celery.utils.log import get_task_logger
 
-
+from api.address.models.address import UserOrderAddress
 from api.celery import app
 
 logger = get_task_logger(__name__)
 
 
 @app.task
-def publish_metadata(message):
-    connection = Connection(settings.BROKER_URL)
-    connection.connect()
-    channel = connection.channel()
-
-    exchange = Exchange('example-exchange', type='direct')
-
-    producer = Producer(
-        channel=channel,
-        routing_key='address-detail',
-        exchange=exchange,
-    )
-    queue = Queue(
-        name='order-address-queue',
-        routing_key='address-detail',
-        exchange=exchange,
-    )
-
-    logger.info('Starting Producer to Send Geo Location to {} queue'.format(queue.name))
-
-    queue.maybe_bind(connection)
-    queue.declare()
-    producer.publish(message)
+def publish_metadata(address_id, geo_location):
+    address = UserOrderAddress.objects.get(id=address_id)
+    address.latitude = geo_location['lat']
+    address.longitude = geo_location['lng']
+    address.save()
