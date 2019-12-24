0235f5ce7f0b89f6a785a0b2b878710ceb147504
Daniel Ribeiro dos Santos <drsantos20@gmail.com>
2019-12-24 18:05:40 -0300

    Update consumer package name

--- api/celery.py
+++ api/celery.py
@@ -3,9 +3,8 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
-app = Celery('api', include=['api.address.tasks'])
+app = Celery('api', include=['api.address.consumer'])
 app.config_from_object('django.conf:settings', namespace='CELERY')
-# app.conf.task_routes = {'api.order.consumer.consumer_from_queue': {'queue:' 'order-customer-queue'}}
 
 
 @app.task(bind=True)
